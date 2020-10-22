fea84a5137403f3366f6874e03c48666a1b46b34
root <root@James-T460.localdomain>
2020-10-22 12:32:50 +0800

    new file:   asset-manifest.json 	new file:   favicon.ico 	new file:   index.html 	new file:   manifest.json 	new file:   precache-manifest.850b1c6f6f0e1435d95d1c7c2148945b.js 	new file:   service-worker.js 	new file:   static/css/2.5861e1ce.chunk.css 	new file:   static/css/2.5861e1ce.chunk.css.map 	new file:   static/css/main.9a9c56f9.chunk.css 	new file:   static/css/main.9a9c56f9.chunk.css.map 	new file:   static/js/2.92270733.chunk.js 	new file:   static/js/2.92270733.chunk.js.LICENSE.txt 	new file:   static/js/2.92270733.chunk.js.map 	new file:   static/js/main.361cdd25.chunk.js 	new file:   static/js/main.361cdd25.chunk.js.map 	new file:   static/js/runtime-main.2e17527d.js 	new file:   static/js/runtime-main.2e17527d.js.map 	new file:   static/media/Digits-JR6M.27ae47ee.ttf 	new file:   static/media/M0n0t3chn0l0gy2407Regular-yP9e.2b2e223f.ttf

---
+++ precache-manifest.850b1c6f6f0e1435d95d1c7c2148945b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01647d174dc7ff58554ffd9041002182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f3d2bb1195b9d28b67c",
+    "url": "/static/css/2.5861e1ce.chunk.css"
+  },
+  {
+    "revision": "18ef2fd438e96fc8479b",
+    "url": "/static/css/main.9a9c56f9.chunk.css"
+  },
+  {
+    "revision": "3f3d2bb1195b9d28b67c",
+    "url": "/static/js/2.92270733.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.92270733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ef2fd438e96fc8479b",
+    "url": "/static/js/main.361cdd25.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  },
+  {
+    "revision": "27ae47eec971c57c0b06c8c33c54a3c5",
+    "url": "/static/media/Digits-JR6M.27ae47ee.ttf"
+  },
+  {
+    "revision": "2b2e223f52f41c7d7871bab7541572d1",
+    "url": "/static/media/M0n0t3chn0l0gy2407Regular-yP9e.2b2e223f.ttf"
+  }
+]);
